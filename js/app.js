f1c57e9c1fd60a2443e669ac9ef9d37941b148c2
Kulsuma Akter <kulsumams72@gmail.com>
2022-08-22 13:25:28 +0600

    select player js part added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('app added')
+let cartArray = []
+function addToCart(element){
+    const playersName = element.parentNode.parentNode.children[0].innerText;
+    console.log(playersName)
+    const olBody = document.getElementById('ol-body');
+    const li = document.createElement('li');
+    li.innerHTML=`
+    <li>${playersName}</li>
+    `;
+    olBody.appendChild(li);
+}
