2a222af47783db2315d797ca2ab3b32c200a5dcb
Kulsuma Akter <kulsumams72@gmail.com>
2022-08-22 21:39:11 +0600

    calculation fuction added

--- js/app.js
+++ js/app.js
@@ -24,6 +24,37 @@ function addToCart(element){
 
 // caculation budget 
 
-document.getElementById('')
+// common function 
+
+function display(elementId){
+    const perPlayerField = document.getElementById(elementId);
+    const perPlayerString = perPlayerField.value;
+    const perPlayer = parseFloat(parseFloat(perPlayerString).toFixed(2));
+
+    return perPlayer;
+
+}
+
+// playerExpenses 
+
+document.getElementById('calculate').addEventListener('click', function(){
+    const parPlayerAmount = display('per-player');
+    const totalExpenses = cartArray.length * parPlayerAmount;
+    const playerExpenses = document.getElementById('player-expenses');
+     playerExpenses.innerText = totalExpenses;
+    
+});
+
+// total Amount
+
+document.getElementById('calculate-total').addEventListener('click', function(){
+    const managerAmount = display('manager-amount');
+    const coachAmount = display('coach-amount');
+    const playerExpensesString = document.getElementById('player-expenses').innerText;
+    const playerExpenses = parseFloat(parseFloat(playerExpensesString).toFixed(2))
+    const totalAmount = managerAmount + coachAmount + playerExpenses;
+    const totalAmountField = document.getElementById('total-amount');
+    totalAmountField.innerText = totalAmount;
+})
 
 
