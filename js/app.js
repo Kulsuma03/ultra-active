77b0e34576f7707ecb81c24fa855f398bfa209a5
Kulsuma Akter <kulsumams72@gmail.com>
2022-08-22 23:19:17 +0600

    more images added

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ let cartArray = [];
 function addToCart(element){
     const playersName = element.parentNode.parentNode.children[0].innerText;
 
-    element.setAttribute('disabled', true);
     const olBody = document.getElementById('myList');
     
     const ol = document.createElement('ol');
@@ -14,12 +13,14 @@ function addToCart(element){
     <li>${cartArray.length + 1}. ${playersName}</li>
     `;
     if(cartArray.length === 5){
+        
         alert('sorry!!!  you can not add more than five');
         return;
     }
+    element.setAttribute('disabled', true);
     olBody.appendChild(ol);
     cartArray.push(playersName);
-   
+    
 }
 
 // caculation budget 
@@ -27,10 +28,10 @@ function addToCart(element){
 // common function 
 
 function display(elementId){
-    const perPlayerField = document.getElementById(elementId);
-    const perPlayerString = perPlayerField.value;
+    const PlayerField = document.getElementById(elementId);
+    const perPlayerString = PlayerField.value;
     const perPlayer = parseFloat(parseFloat(perPlayerString).toFixed(2));
-
+    PlayerField.value = '';
     return perPlayer;
 
 }
@@ -39,6 +40,10 @@ function display(elementId){
 
 document.getElementById('calculate').addEventListener('click', function(){
     const parPlayerAmount = display('per-player');
+    if(isNaN(parPlayerAmount)){
+        alert('please provid a valid number');
+        return;
+    }
     const totalExpenses = cartArray.length * parPlayerAmount;
     const playerExpenses = document.getElementById('player-expenses');
      playerExpenses.innerText = totalExpenses;
@@ -48,13 +53,17 @@ document.getElementById('calculate').addEventListener('click', function(){
 // total Amount
 
 document.getElementById('calculate-total').addEventListener('click', function(){
+
     const managerAmount = display('manager-amount');
     const coachAmount = display('coach-amount');
     const playerExpensesString = document.getElementById('player-expenses').innerText;
     const playerExpenses = parseFloat(parseFloat(playerExpensesString).toFixed(2))
     const totalAmount = managerAmount + coachAmount + playerExpenses;
     const totalAmountField = document.getElementById('total-amount');
+    if(isNaN(managerAmount) || isNaN(coachAmount) || isNaN(parPlayerAmount)){
+        alert('please provid a valid number');
+        return;
+    }
     totalAmountField.innerText = totalAmount;
 })
-
 
